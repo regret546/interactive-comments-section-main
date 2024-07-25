21a8ec979036343a56e15f06e74efca1abf8bcb0
regret546 <iregret546@gmail.com>
2024-07-25 23:04:21 +0800

    add tailwind packages

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"], // Add your desired output folder path here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+/** @type {import('tailwindcss').Config} */
+/* module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; */
