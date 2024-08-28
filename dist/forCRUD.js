eb8d4e947e7604af605d9e095cc6d621a4318e91
regret546 <iregret546@gmail.com>
2024-08-29 01:22:49 +0800

    initial creation of create reply

--- dist/forCRUD.js
+++ dist/forCRUD.js
@@ -31,56 +31,72 @@ document.addEventListener("dataLoaded", function () {
         bodyElement.classList.remove("delete");
       }
     });
+  });
 
-    /* for updating reply of current-user */
-    const editButtons = document.querySelectorAll("#editBtn");
-    editButtons.forEach((editBtn) => {
-      editBtn.addEventListener("click", function (e) {
-        e.preventDefault();
-        const currentUserReplyContainer = this.closest("#current-user-post");
-        const editReplyContainer =
-          currentUserReplyContainer.querySelector("#forEdit");
-        const currentUserReplyText = editReplyContainer.querySelector(
-          "[reply-data-user-reply]"
-        );
+  /* for updating reply of current-user */
+  const editButtons = document.querySelectorAll("#editBtn");
+  editButtons.forEach((editBtn) => {
+    editBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      const currentUserReplyContainer = this.closest("#current-user-post");
+      const editReplyContainer =
+        currentUserReplyContainer.querySelector("#forEdit");
+      const currentUserReplyText = editReplyContainer.querySelector(
+        "[reply-data-user-reply]"
+      );
 
-        if (editReplyContainer.classList.contains("update")) {
-          editBtn.classList.remove("pointer-events-none");
-          editReplyContainer.classList.remove("update");
-          currentUserReplyText.setAttribute("contenteditable", "false");
-        } else {
-          editReplyContainer.classList.add("update");
-          editBtn.classList.add("pointer-events-none");
-          editBtn.classList.add("update");
-          currentUserReplyText.setAttribute("contenteditable", "true");
-          currentUserReplyText.id = "editable-content";
-        }
+      if (editReplyContainer.classList.contains("update")) {
+        editBtn.classList.remove("pointer-events-none");
+        editReplyContainer.classList.remove("update");
+        currentUserReplyText.setAttribute("contenteditable", "false");
+      } else {
+        editReplyContainer.classList.add("update");
+        editBtn.classList.add("pointer-events-none");
+        editBtn.classList.add("update");
+        currentUserReplyText.setAttribute("contenteditable", "true");
+        currentUserReplyText.id = "editable-content";
+      }
 
-        // automatically place the cursor at the end of the content once //
-        document.getElementById("editable-content").addEventListener(
-          "click",
-          function (event) {
-            const selection = window.getSelection();
-            const range = document.createRange();
-            range.selectNodeContents(this);
-            range.collapse(false);
-            selection.removeAllRanges();
-            selection.addRange(range);
-          },
-          { once: true }
-        );
+      // automatically place the cursor at the end of the content once //
+      document.getElementById("editable-content").addEventListener(
+        "click",
+        function (event) {
+          const selection = window.getSelection();
+          const range = document.createRange();
+          range.selectNodeContents(this);
+          range.collapse(false);
+          selection.removeAllRanges();
+          selection.addRange(range);
+        },
+        { once: true }
+      );
 
-        // for update button //
-        const updateButton = document.querySelector("#updateBtn");
-        updateButton.addEventListener("click", function () {
-          editBtn.classList.remove("pointer-events-none");
-          editBtn.classList.remove("update");
-          editReplyContainer.classList.remove("update");
-          currentUserReplyText.setAttribute("contenteditable", "false");
-          currentUserReplyText.id = "";
-          currentUserReplyText.innerText = currentUserReplyText.textContent;
-        });
+      // for update button //
+      const updateButton = document.querySelector("#updateBtn");
+      updateButton.addEventListener("click", function () {
+        editBtn.classList.remove("pointer-events-none");
+        editBtn.classList.remove("update");
+        editReplyContainer.classList.remove("update");
+        currentUserReplyText.setAttribute("contenteditable", "false");
+        currentUserReplyText.id = "";
+        currentUserReplyText.innerText = currentUserReplyText.textContent;
       });
+    });
+  });
+
+  /* for creating reply of current-user */
+
+  //For Comment Reply //
+  const commentReply = document.querySelectorAll("#comment-reply");
+  commentReply.forEach((reply) => {
+    reply.addEventListener("click", function () {
+      const commentSection = reply.closest("#comment-section");
+      const commentField = commentSection.querySelector("#comment-field");
+      if (commentField) {
+        console.log('The element "comment-field" exists.');
+      } else {
+        console.log('The element "comment-field" does not exist.');
+      }
     });
   });
 });
